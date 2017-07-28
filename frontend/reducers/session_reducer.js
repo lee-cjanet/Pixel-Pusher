071ed5ab1134b6a16aff4cd318d55c854ff9607a
Janet Lee <lee.cjanet@gmail.com>
2017-07-27 20:00:02 -0700

    currentUser id bug fixed

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -2,7 +2,7 @@ import merge from 'lodash/merge';
 
 import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 
-const defaultState = {currentUser:null};
+const defaultState = {currentUser: {} };
 
 const SessionReducer = (state = defaultState, action) => {
   Object.freeze(state);

2bae535247dbdba90873867b05557b9ef5c3fbde
Janet Lee <lee.cjanet@gmail.com>
2017-10-14 22:51:32 -0700

    start profile edit form

---
+++ frontend/components/user_profile/edit_profile/edit_form_container.js
@@ -0,0 +1,19 @@
+// import { connect } from 'react-redux';
+//
+// import EditForm from './edit_form';
+//
+//
+// const mapStateToProps = (state, { match }) => {
+//   // return {
+//   //   images: selectAllUserPhotos(state.user_profile)
+//   // };
+// };
+//
+// const mapDispatchToProps = dispatch => ({
+//   // fetchUserPhotos: (id) => dispatch(fetchUserPhotos(id))
+// });
+//
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+)(EditForm);

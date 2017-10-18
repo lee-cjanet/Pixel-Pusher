fa3270e25c695f3e58d847971ea1789576bb9bcd
Janet Lee <lee.cjanet@gmail.com>
2017-10-17 19:31:22 -0700

    mapstatetoProps editProfile

--- frontend/components/user_profile/edit_profile/edit_form_container.js
+++ frontend/components/user_profile/edit_profile/edit_form_container.js
@@ -1,19 +1,30 @@
-// import { connect } from 'react-redux';
-//
-// import EditForm from './edit_form';
-//
-//
-// const mapStateToProps = (state, { match }) => {
-//   // return {
-//   //   images: selectAllUserPhotos(state.user_profile)
-//   // };
-// };
-//
-// const mapDispatchToProps = dispatch => ({
-//   // fetchUserPhotos: (id) => dispatch(fetchUserPhotos(id))
-// });
-//
-// export default connect(
-//   mapStateToProps,
-//   mapDispatchToProps
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router'
+
+import EditForm from './edit_form';
+import { updateUser } from '../../../actions/profile_actions';
+
+
+const mapStateToProps = (state, { match }) => {
+  return {
+    name: state.user_profile.user.name,
+    bio: state.user_profile.user.bio,
+    cover_url: state.user_profile.user.cover_url,
+    headshot_url: state.user_profile.user.headshot_url
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  // fetchUserPhotos: (id) => dispatch(fetchUserPhotos(id))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
 )(EditForm);
+
+
+
+const mapDispatchToProps = dispatch => ({
+  updateUser: (user, id) => dispatch(updateUser(user, id)),
+  receiveUserErrors: (errors) => dispatch(receiveUserErrors(errors))
